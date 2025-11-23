a1dbb0cedf8b85291b5211c3c41f15fab03a46f7
Leyva Bezerra <leyvinobezerra.develop@gmail.com>
2025-11-22 23:42:46 -0300

    MPI

---
+++ MPI.c
@@ -0,0 +1,15 @@
+_____________________________________________________________
+_____________________________________________________________
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+* Messege Passing inteface(MPI)
+       * É uma interface de progrmação baseada no modelo
+         de trocas de mensagens
+       * Um padrão define sua interface e suas funcionalidade
+
+
+* MPI- Forum
+       
+       * Define a padronização da interfaces
+       * Discute inovação e novas direções
+       * Verificar a corretude e qualidade do padrão 
